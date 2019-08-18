11974521ee4d1ac29cfbec0057a93149fed6c8af
anurag muthyam <amuthyam@rccl.com>
2019-08-18 14:39:12 -0400

    implemented MovieCost test

--- src/main/java/com/micro_service/workflows/JsonPayloadWorkflow.java
+++ src/main/java/com/micro_service/workflows/JsonPayloadWorkflow.java
@@ -7,12 +7,20 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 import static com.micro_service.workflows.ConstantsWorkflow.MENU_SERVICE_PATH;
+import static com.micro_service.workflows.ConstantsWorkflow.MOVIE_SERVICE_PATH;
 
 public class JsonPayloadWorkflow {
     
     public static JsonElement retrieveMenuServiceDoc() throws FileNotFoundException {
-        
-        FileReader reader = new FileReader(MENU_SERVICE_PATH);
+        return retrieveServiceDocument(MENU_SERVICE_PATH);
+    }
+    
+    public static JsonElement retrieveMoviesServiceDoc() throws FileNotFoundException {
+        return retrieveServiceDocument(MOVIE_SERVICE_PATH);
+    }
+    
+    private static JsonElement retrieveServiceDocument(String servicePath) throws FileNotFoundException {
+        FileReader reader = new FileReader(servicePath);
         JsonParser jsonParser = new JsonParser();
         return jsonParser.parse(reader);
     }
---
+++ src/test/java/com/micro_service/regression/movies/MovieCost.java
@@ -0,0 +1,63 @@
+package com.micro_service.regression;
+
+import base.SuperClass;
+import com.google.gson.JsonElement;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.StringUtils;
+
+import java.io.FileNotFoundException;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Stream;
+
+import static com.micro_service.workflows.JsonPayloadWorkflow.retrieveMoviesServiceDoc;
+import static com.micro_service.workflows.JsonWorkflow.getJsonStream;
+import static com.micro_service.workflows.JsonWorkflow.getJsonString;
+
+public class MovieCost extends SuperClass {
+    
+    /**
+     * Test budget and boxOffice of the movies
+     * -> If budget, and boxOffice are null or empty, it is fine.
+     * -> If budget or boxOffice is null or empty, the test case FAILS
+     */
+    @Test
+    public void testBudgetAndBoxOfficeAreNullOrEmptyForMovies() throws FileNotFoundException {
+        
+        Stream<JsonElement> movies = getJsonStream(retrieveMoviesServiceDoc(), "payload.movies");
+        
+        AtomicBoolean isCostFieldNull = new AtomicBoolean(false);
+        
+        movies.forEach(movie -> {
+            
+            String movieTitle = getJsonString(movie, "title");
+            String budget = getJsonString(movie, "cost.budget");
+            String boxOffice = getJsonString(movie, "cost.boxOffice");
+            
+            if (StringUtils.isBlank(budget) || StringUtils.isBlank(boxOffice)) {
+                
+                // if budget and boxOffice are all null, then skip.
+                if (StringUtils.isBlank(budget) && StringUtils.isBlank(boxOffice)) {
+                    isCostFieldNull.set(false);
+                }
+                // if budget or boxOffice is null or empty, the test case fails.
+                else {
+                    if (StringUtils.isBlank(budget)) {
+                        isCostFieldNull.set(true);
+                        log("budget: is null or empty for movieTitle: %s ", movieTitle);
+                    }
+                    if (StringUtils.isBlank(boxOffice)) {
+                        isCostFieldNull.set(true);
+                        log("boxOffice: is null or empty for movieTitle: %s", movieTitle);
+                    }
+                }
+            }
+        });
+        
+        if (isCostFieldNull.get()) {
+            log("FAILED");
+            Assertions.fail();
+        }
+    }
+    
+}
