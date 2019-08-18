0e8a0fe57d265ca0cef186f536f224b6a26362e3
anurag muthyam <amuthyam@rccl.com>
2019-08-18 11:53:54 -0400

    added description for section name

--- src/test/java/com/micro_service/regression/menus/SectionName.java
+++ src/test/java/com/micro_service/regression/menus/SectionName.java
@@ -19,6 +19,12 @@ public class SectionName extends SuperClass {
      * Procedure 1: Test if the sectionName is null
      * If the sectionName == null, then the test FAILS
      * If the sectionName != null then the test PASSES
+     * <p>
+     * -> In Procedure 1, we are passing the json doc and a json field to getJsonStream()
+     * -> Then we are applying flatMap() operator to go into the menuSections in the json file
+     * -> And we are filtering to check the sectionName is null or not
+     * -> findAny() helps to find sectionName = null
+     * -> And finally returns the boolean value true or false by the terminal operator: isPresent();
      */
     @Test
     public void findMenuSectionNameIsNullProcedure1() throws FileNotFoundException {
@@ -38,6 +44,11 @@ public class SectionName extends SuperClass {
      * Procedure 2: Test if the sectionName is null
      * If the sectionName == null, then the test FAILS
      * If the sectionName != null then the test PASSES
+     * <p>
+     * -> In Procedure 2, we are passing the json doc and a json field to getJsonStream()
+     * -> Then we are applying forEach() operator to iterate each and every menuSections
+     * -> If menuSections = null then peek() operator prints out menuSections = null
+     * -> Then we are using the terminal operator: count() to get total count of menuSections which are null
      */
     @Test
     public void findMenuSectionNameIsNullProcedure2() throws FileNotFoundException {
